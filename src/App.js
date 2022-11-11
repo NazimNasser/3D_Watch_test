e6f1b62fe8b947e1c46aab6d74d1bd6fa5f9e368
NazimNasser <nazimnasser94@gmail.com>
2022-11-12 00:24:39 +0200

    add rotation for model

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import { ContactShadows, Float, OrbitControls, Stars } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css';
 import * as THREE from 'three'
 import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react';
+
 
 function Watch(props) {
-
+  
+  const myMesh = useRef();
+  
+  useFrame(() => {
+    const a = window.pageYOffset / 500
+    myMesh.current.rotation.y = a;
+  });
   const { nodes, materials } = useGLTF('watch/vintage_pocket_watch_4k.gltf')
   return (
-    <group {...props} dispose={null} scale={50}>
+    <group {...props} dispose={null} scale={50}  ref={myMesh}>
       <mesh geometry={nodes.Circle001.geometry} material={materials.vintage_pocket_watch_glass} />
       <mesh geometry={nodes.Circle001_1.geometry} material={materials.vintage_pocket_watch} />
       <mesh geometry={nodes.Circle001_2.geometry} material={materials.vintage_pocket_watch_hands} />
@@ -28,32 +36,21 @@ function Plane() {
   )
 }
 
-function onMouseWheel(event) {
-
-  event.preventDefault();
-
-  Watch.rotation.y += event.deltaY * 0.05;
-
-}
-
 function App() {
-  // window.addEventListener('wheel', onMouseWheel, true);
+
   return (
-    <div className='bg-[#161616]'>
-      {/* <div className='text-white font-bold text-[20px]'>2D</div> */}
+    <div className='!min-h-[100vh] sticky top-0 bg-[#161616]'>
       <Canvas>
         <OrbitControls
           minPolarAngle={Math.PI / 2}
           maxPolarAngle={Math.PI / 2}
-          autoRotate={true}
           autoRotateSpeed={1.1}
           minDistance={4}
           maxDistance={5}
-          // reverseOrbit={true}
-          // enablePan={false}
-          // enableZoom={false}
+          enablePan={false}
+          enableZoom={false}
           mouseButtons={{
-            LEFT: THREE.MOUSE.ROTATE,
+            LEFT: '',
             MIDDLE: '',
             RIGHT: ''
           }}
@@ -63,7 +60,7 @@ function App() {
         <pointLight position={[50, 50, 50]} intensity={1} />
         <pointLight position={[-30, -30, 20]} intensity={1} />
         <Float speed={1.4} rotationIntensity={1} floatIntensity={1.3}>
-          <Watch/>
+          <Watch />
         </Float>
         <ContactShadows position={[0, -1.9, 0]} blur={2} scale={20} far={5} color='#fff' />
         {/* <Plane /> */}
